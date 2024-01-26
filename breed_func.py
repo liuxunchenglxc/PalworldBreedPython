c19e005265885a21728fa4c9f281350f9f4edadb
刘迅承 <33124039+liuxunchenglxc@users.noreply.github.com>
2024-01-26 17:46:20 +0800

    Add files via upload

---
+++ breed_func.py
@@ -0,0 +1,137 @@
+
+
+
+class NameNum:
+    def __init__(self, csv_path='palworld_name2num.csv'):
+        # load csv
+        self.name2num={}
+        self.num2name={}
+        with open(csv_path, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            line = line.strip('\ufeff')
+            name, num = line.split(',')
+            self.name2num[name] = num
+            self.num2name[num] = name
+
+    def get_name(self, num: str):
+        return self.num2name[num]
+
+    def get_num(self, name: str):
+        return self.name2num[name]
+
+class BreedEquals:
+    def __init__(self, csv_path='palworld_breed.csv'):
+        # load csv
+        self.offsprings = {}
+        self.parents = {}
+        with open(csv_path, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            line = line.strip('\n')
+            line = line.strip('\ufeff')
+            p1, p2, off = line.split(',')
+            if p1 in self.offsprings:
+                self.offsprings[p1][p2] = off
+            else:
+                self.offsprings[p1] = {p2: off}
+            if p2 in self.offsprings:
+                self.offsprings[p2][p1] = off
+            else:
+                self.offsprings[p2] = {p1: off}
+            self.parents[off] = (p1, p2)
+
+    def get_all_offspring(self, p1: str):
+        if p1 in self.offsprings:
+            return self.offsprings[p1]
+        else:
+            return None
+
+    def get_offspring(self, p1: str, p2: str):
+        if p1 in self.offsprings:
+            if p2 in self.offsprings[p1]:
+                return self.offsprings[p1][p2]
+        return None
+
+
+class Closure:
+    def __init__(self):
+        self.closure = {}
+        self.add_times = 0
+        self.size = 0
+        self.be = BreedEquals()
+        self.nn = NameNum()
+
+    def add_pals(self, num_list):
+        for num in num_list:
+            num = str(num)
+            self.closure[num] = {'dist': self.add_times}
+            self.size += 1
+        self.add_times += 1
+        if self.size > 1:
+            tmp_size = 0
+            while tmp_size < self.size:
+                tmp_size = self.size
+                for p1 in list(self.closure.keys()):
+                    for p2 in list(self.closure.keys()):
+                        off = self.be.get_offspring(p1, p2)
+                        if off and off not in self.closure:
+                            self.closure[off] = {'dist': self.add_times, 'path': (p1, p2)}
+                            self.size += 1
+                self.add_times += 1
+
+    def get_closure_all_num(self):
+        return list(self.closure.keys())
+
+    def get_closure_all_name(self):
+        return [self.nn.get_name(num) for num in list(self.closure.keys())]
+
+    def get_closure_key_as_name(self):
+        r = {}
+        for k, v in list(self.closure.items()):
+            v['num'] = k
+            r[self.nn.get_name(k)] = v
+        return r
+
+    def check_num(self, num):
+        num = str(num)
+        if num in self.closure:
+            return True
+        else:
+            return False
+
+    def num_path(self, num):
+        num = str(num)
+        r = []
+        check_list = [num]
+        checked_list = []
+        while check_list:
+            off = check_list.pop()
+            if 'path' in self.closure[off] and off not in checked_list:
+                p1, p2 = self.closure[off]['path']
+                check_list.append(p1)
+                check_list.append(p2)
+                r.append((off,p1,p2))
+            checked_list.append(off)
+        return r
+
+    def path2string(self, path):
+        if not path:
+            return 'This is original Pal.'
+        s = ''
+        for off, p1, p2 in path:
+            num2name = lambda x: self.nn.get_name(x)
+            s += f'{num2name(off)} <- {num2name(p1)} + {num2name(p2)}\n'
+        return s
+
+if __name__ == '__main__':
+    #nn = NameNum()
+    # print(nn.name2num, nn.num2name)
+    #be = BreedEquals()
+    # print(be.offsprings, be.parents)
+    c = Closure()
+    c.add_pals([1,2,3,4,5,11,12,13,14,15])
+    print(c.get_closure_key_as_name())
+    print(c.check_num(86), c.check_num(56))
+    print(c.path2string(c.num_path(64)))
